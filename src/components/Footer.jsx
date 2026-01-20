6ec88d9693b4d2fd336d8438e10675316749b1d6
impacgo <adityaramipg123@gmail.com>
2026-01-20 18:06:41 +0530

    Fix footer layout and update container class

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ export default function Footer(){
     return (
         <div>
             <footer className="bg-gray-800 text-white py-8">
-        <div className="container mx-auto px-4 text-center">
+        <div className="container mx-auto px-4 ">
+            <div className="flex flex-col items-center justify-center gap-3 text-center">
           <iframe id='Iframe1' src='https://dunsregistered.dnb.com/SealAuthentication.aspx?Cid=1' width='114px' height='97px' frameborder='0' scrolling='no' allowtransparency='true' ></iframe>
           <p>&copy; 2024 Impacgo Solutions. All rights reserved.</p>
+            </div>
         </div>
       </footer>
         </div>
